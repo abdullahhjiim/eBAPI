ecc2bfedb9ba9759c4959287b9d1442c2ad11591
jim <abdullahalhasan.cse@gmail.com>
2024-03-01 17:03:32 +0400

    Initial Project

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  redirects:async()=> {
+    return [
+      {
+        source:'/',
+        destination:'/login',
+        permanent:false
+      }
+    ]
+  },
+  images: {
+    // domains: ['i.ibb.co','res.cloudinary.com','lh3.googleusercontent.com'],
+    domains: ['media.istockphoto.com','freepngimg.com','e7.pngegg.com','encrypted-tbn0.gstatic.com'],
+  },
+}
+
+module.exports = nextConfig
